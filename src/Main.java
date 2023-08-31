aac8f0289afdd02b691e1fb3efc8ad9b7377dbd6
Alex <alex97av@gmail.com>
2023-08-31 12:32:27 +0200

    fine

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
-/**
- * Exercise: Enumerations
- * create an enumeration Month with all the name of the months
- * iterate all the values of the enumeration, printing the name of the month followed by:
- * ends with y if the name ends with Y
- * doesn't end with y if the name doesn't end with y
- */
-
 public class Main {
+    public static void main(String[] args) {
+        enum months {January, February, March, April, May, June, July, August, September, October, November, December}
+        for (months element : months.values()) {
+            if (element.toString().endsWith("y")) {
+                System.out.println(element + " ends with y");
+            } else System.out.println(element + " doesn't end with y");
+        }
+    }
 }
