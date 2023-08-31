d7052054c99517d3cc78809845bb1dac1dce6b65
Alex <alex97av@gmail.com>
2023-08-31 12:46:48 +0200

    some fix

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        enum months {January, February, March, April, May, June, July, August, September, October, November, December}
-        for (months element : months.values()) {
-            if (element.toString().endsWith("y")) {
+
+        for (Months element : Months.values()) {
+            if (element.toString().endsWith("Y")) {
                 System.out.println(element + " ends with y");
             } else System.out.println(element + " doesn't end with y");
         }
